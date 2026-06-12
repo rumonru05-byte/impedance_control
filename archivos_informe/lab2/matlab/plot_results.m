6cf0f3c73ead1869e4095a65af705721c0376a4f
rumonru05-byte <rubenmontero@uma.es>
2026-06-12 12:48:53 +0200

    Añadidos archivos para el informe final.

--- archivos_informe/lab2/matlab/plot_results.m
+++ archivos_informe/lab2/matlab/plot_results.m
@@ -1,8 +1,8 @@
 %% Get experiment data
 
-get_position_data_b05;
-get_velocity_data_b05;
-get_acceleration_data_b05;
+get_position_data;
+get_velocity_data;
+get_acceleration_data;
 
 joint_position = fillmissing(joint_position, 'linear');
 joint_velocity = fillmissing(joint_velocity, 'linear');
@@ -18,7 +18,7 @@ plot(joint_position(:,1)-joint_position(1,1), joint_position(:,3), 'LineWidth',1
 ylabel('angular position $[rad]$','interpreter','latex', 'fontSize',14);
 xlabel('time [s]','interpreter','latex', 'fontSize',14);
 legend('$\theta_1$','$\theta_2$','interpreter','latex', 'fontSize',14);
-xlim([0 15])
+xlim([0 joint_position(end,1)-joint_position(1,1)])
 grid on
 grid minor
 
@@ -31,7 +31,7 @@ plot(joint_velocity(:,1)-joint_velocity(1,1), joint_velocity(:,3), 'LineWidth',1
 ylabel('angular velocity $[rad/s]$','interpreter','latex', 'fontSize',14);
 xlabel('time [s]','interpreter','latex', 'fontSize',14);
 legend('$\dot{\theta}_1$','$\dot{\theta}_2$','interpreter','latex', 'fontSize',14);
-xlim([0 15])
+xlim([0 joint_velocity(end,1)-joint_velocity(1,1)])
 grid on
 grid minor
 
@@ -45,12 +45,12 @@ plot(joint_acceleration(:,1)-joint_acceleration(1,1), joint_acceleration(:,3), '
 ylabel('angular acceleration $[rad/s^2]$','interpreter','latex', 'fontSize',14);
 xlabel('time [s]','interpreter','latex', 'fontSize',14);
 legend('$\ddot{\theta_1}$','$\ddot{\theta_2}$','interpreter','latex', 'fontSize',14);
-xlim([0 15])
+xlim([0 joint_acceleration(end,1)-joint_acceleration(1,1)])
 grid on
 grid minor
 
 %% Guardado SVG
-ruta_destino = '\\wsl.localhost\Ubuntu-22.04\home\ruben\ros\amp_rob_ws\src\uma_arm_control\archivos_informe\graficas_dinamica_lab2_b05.svg';
+ruta_destino = '\\wsl.localhost\Ubuntu-22.04\home\ruben\ros\amp_rob_ws\src\uma_arm_control\archivos_informe\lab2\img\graficas_dinamica.svg';
 
 exportgraphics(gcf, ruta_destino, 'ContentType', 'vector');
 
